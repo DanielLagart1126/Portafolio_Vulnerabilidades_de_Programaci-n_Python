78d59ba030ee9d9ae7270bfafd5acd4839fbb9b7
Daniel Bustamante Lagart <109637835+DanielLagart1126@users.noreply.github.com>
2022-07-25 08:09:36 -0500

    SEMANA_2
    En esta semana se repaso los conceptos básicos sobre seudocódigos

---
+++ SEMANA_2/Dia1 Variables y Constantes.py
@@ -0,0 +1,9 @@
+# Codigo Dia uno
+# Realizar un codigo que te muestre tu nombre y edad
+# Inicio de codigo
+print("Dime tu nombre:")#Print es para escribir el panatalla
+nombreUsuario=input()#Input es para poder recibir un dato del Usuario
+print("Dime tu Edad:")
+edadUsuario=input()
+print("Muy buenos Días "+nombreUsuario+" ya estas grandote tienes "+edadUsuario+".")
+# Final de codigo
